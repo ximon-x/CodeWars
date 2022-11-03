c240a77866fc660a8ac9091d128e2f0ac5cfdfd5
Simon Samuel <salviensky@gmail.com>
2022-11-03 08:52:38 +0100

    Have algorithm for solving the problem

---
+++ go/are_they_the_same.go
@@ -0,0 +1,17 @@
+package kata
+
+func Comp(array1 []int, array2 []int) bool {
+	// if a or b is nil, return false
+
+	// Iterate through array 1
+	// 		found = false
+
+	// 		Iterate through array 2
+	// 			if item in array 2 is multiple  of  array 1 item
+	//     			found = true
+	
+	// 		if found = false return false
+
+
+	// return true
+}
