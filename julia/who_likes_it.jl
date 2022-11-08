5b98fccd7c347bc998c5e92d4219d800578ff79b
Simon Samuel <salviensky@gmail.com>
2022-11-08 10:57:24 +0100

    Solved the who_likes_it (Julia) kata

--- julia/who_likes_it.jl
+++ julia/who_likes_it.jl
@@ -1,7 +1,23 @@
-module kata
-	export likes
+module Kata
+export likes
 
-	function likes(names::Vector{String}) :: String 
+function likes(names::Vector{String})::String
+    if (length(names) == 0)
+        return "no one likes this"
 
-	end
+    elseif (length(names) == 1)
+        return "$(names[1]) likes this"
+
+    elseif (length(names) == 2)
+        return "$(names[1]) and $(names[2]) like this"
+
+    elseif (length(names) == 3)
+        return "$(names[1]), $(names[2]) and $(names[3]) like this"
+
+    else
+        return "$(names[1]), $(names[2]) and $(length(names) - 2) others like this"
+    end
 end
+println(likes(["Peter", "Paul", "Parrot"]))
+end
+
