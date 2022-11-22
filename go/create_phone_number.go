ff83682d2029665bdfc5b92987dab15d33b3cc21
Simon Samuel <salviensky@gmail.com>
2022-11-22 08:58:28 +0100

    Solved the create_phone_number kata (Go)

---
+++ go/create_phone_number.go
@@ -0,0 +1,27 @@
+package kata
+
+import "fmt"
+
+func CreatePhoneNumber(numbers [10]uint) string {
+	result := ""
+	numIndex := 0
+
+	for i := 0; i < 14; i++ {
+		if i == 0 {
+			result += "("
+
+		} else if i == 4 {
+			result += ")"
+		} else if i == 5 {
+			result += " "
+		} else if i == 9 {
+			result += "-"
+		} else {
+			result += fmt.Sprint(numbers[numIndex])
+			numIndex += 1
+		}
+
+	}
+
+	return result
+}
