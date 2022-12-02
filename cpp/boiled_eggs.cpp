e0cb574daa303c0f58ce3b60a8c8f6582e025dd5
Simon Samuel <salviensky@gmail.com>
2022-12-02 08:52:29 +0100

    Solved the boiled_eggs (C++) kata

---
+++ cpp/boiled_eggs.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+
+unsigned int cookingTime (unsigned int eggs) {
+	unsigned int total_time = 0;
+
+	while (eggs > 0) {
+		if (eggs < 8) {
+			total_time = total_time + 5;
+			eggs = eggs - eggs;
+		}
+		else {
+			total_time = total_time + 5;
+			eggs = eggs - 8;
+		}
+	}
+
+	return total_time;
+}
+
+int main () {
+	cout << cookingTime(5) << endl;
+
+	return 0;
+}
