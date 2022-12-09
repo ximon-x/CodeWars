313575a86d517d88133f3c687dc015a143f16b2a
Simon Samuel <salviensky@gmail.com>
2022-12-09 08:58:59 +0100

    Solved the convert_string_to_camel_case (Lua) Kata

---
+++ lua/convert_string_to_camel_case.lua
@@ -0,0 +1,25 @@
+return function(str)
+    local result = ""
+
+    if str:len() == 0 then return result end
+
+    local flag = false
+
+
+    for c in str:gmatch "." do
+
+        if c == "-" or c == "_" then
+            flag = true
+
+        elseif flag == true then
+            result = result .. string.upper(c)
+            flag = false
+
+        else
+            result = result .. c
+
+        end
+    end
+
+    return result
+end
