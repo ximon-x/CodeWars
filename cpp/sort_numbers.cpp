700e20a759058a2ffe34ef2f1bd9fa733c365f75
Simon Samuel <salviensky@gmail.com>
2023-01-21 08:19:04 +0100

    feat: solved sort_numbers kata(cpp)

--- cpp/sort_numbers.cpp
+++ cpp/sort_numbers.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 
 std::vector<int> solution(std::vector<int> nums) {
-  std::sort(nums);
+  int n = sizeof(nums) / sizeof(nums[0]);
+
+  std::sort(nums.begin(), nums.end());
   return nums;
 }
